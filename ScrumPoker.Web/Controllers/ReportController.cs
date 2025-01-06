3638284326163c9365d800201fcdab5d8bfed74f
DSANS\osarikaya <ozgur.sarikaya@nesine.com>
2025-01-06 23:47:08 +0300

    Rapor sayfaları

--- ScrumPoker.Web/Controllers/ReportController.cs
+++ ScrumPoker.Web/Controllers/ReportController.cs
@@ -5,8 +5,13 @@ namespace ScrumPoker.Web.Controllers
     [Route("report")]
     public class ReportController : Controller
     {
-        [Route("list", Name = "ReportIndex")]
-        public IActionResult Index()
+        [Route("planing-poker-reports", Name = "PlaningPokerReport")]
+        public IActionResult PlaningPokerReport()
+        {
+            return View();
+        }
+        [Route("sprint-retrospective-reports", Name = "SprintRetrospectiveReport")]
+        public IActionResult SprintRetrospectiveReport()
         {
             return View();
         }
